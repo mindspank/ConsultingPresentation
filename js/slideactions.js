ecab4e9acb0fe72ac83a120d068d97743dedd762
Alexander Karlsson <alexander.karlsson@qlik.com>
2016-02-17 16:50:13 -0500

    .

--- js/slideactions.js
+++ js/slideactions.js
@@ -1,6 +1,8 @@
 /* global Reveal */
 'use strict';
 
+/*
+
 var config = {
 	host: 'localhost',
 	prefix: '/',
@@ -11,13 +13,10 @@ window.require.config({
 	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port: "") + config.prefix + "resources"
 });
 
-Reveal.addEventListener('testslide', function() {
-    require(["js/qlik"], function ( qlik ) {
-        var app = qlik.openApp('TestScript.qvf', config);
-        app.getObject('mashup','kEFjgJ');
-    });
-});
+*/
 
-define([''], function() {
-    
-})
+Reveal.addEventListener('testslide', function() {
+
+    console.log('test')
+
+});
