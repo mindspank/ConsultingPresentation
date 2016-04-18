1b3bb55f18344e10682e93b80dbcd7acc15ea503
Alexander Karlsson <alexander.karlsson@qlik.com>
2016-04-17 20:09:19 -0400

    new slides

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 define([], function() {
    return {
        apps: {
-           search: '433ab157-6fd4-415d-89cd-2af1b925bc5a',
+           search: '3c6bd21f-7447-41ed-94af-535c22682aeb',
            extension: 'd9441e33-f490-4163-9ac8-636927f3b353'
        },
        qlik: {
-           host: 'branch.qlik.com',
+           host: 'localhost',
            isSecure: true,
            prefix: "/",
            port: 443
